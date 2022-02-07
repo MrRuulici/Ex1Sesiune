7b8e4c83e69a196c689667c73f850e6c5b9df5a1
Raul Ulici <raululici@yahoo.com>
2022-02-07 10:02:01 +0200

    Final

---
+++ Ex1Sesiune/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex1Sesiune
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            int i;
+            for (i = 0; i < a.Length; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(Recursivitate.Suma(a, a.Length - 1));
+        }
+    }
+}
---
+++ Ex1Sesiune/recursivitate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1Sesiune
+{
+    public class Recursivitate
+    {
+        public static int Suma(int[] a, int n)
+        {
+            if (n == 0)
+                return a[n];
+            else
+                return a[n] + Suma(a, n - 1);
+        }
+        
+    }
+}
